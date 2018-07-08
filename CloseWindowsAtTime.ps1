7ec79992d2f943a3354d623c45d29598a25e2c42
Michal <michal.flisikowski01@gmail.com>
2018-07-08 14:14:51 +0200

    Add files via upload

---
+++ CloseWindowsAtTime.ps1
@@ -0,0 +1,12 @@
+﻿$TimeToOff = Read-Host "Enter time in 24 format that PC should shut down [xx:xx]"
+Write-Host "Now computer will be closed automatically."
+Write-Host "Waiting..."
+$TimeNow = Get-Date -Format HH:mm
+While(Compare-Object -ReferenceObject $TimeToOff $TimeNow)
+{
+   Start-Sleep -s 60 
+   $TimeNow = Get-Date -Format HH:mm
+}
+Write-Host "Turning off... Goodbye! :)" -ForegroundColor red
+Start-Sleep -s 15 
+Stop-Computer
